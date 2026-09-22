9ccdea36b8be0d517ac5c317788a423e9347d2a9
Shruti Pawar <shruti.pawar8380@gmail.com>
2026-09-22 19:27:15 +0530

    change

---
+++ linked List/singly linked list/bubblesort.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node 
+{
+    int data;
+    struct node *next;
+
+};
+struct node *START;
+void insert()
+{
+    int value;
+    struct node *p;
+    p=malloc(sizeof(struct node));
+    printf("ENter the node data :");
+    scanf("%d",&value);
+    p->data=value;
+    p->next=NULL;
+    if(START==NULL)
+    {
+        START=p;
+    }
+    else
+    {
+        p->next=START;
+        START=p;
+    }
+
+}
+
+void display()
+{
+    struct node *t;
+    if(START== NULL)
+    {
+        printf("The Linked List is Empty");
+    }
+    else
+    {
+        t=START;
+        printf("\nThe Linked List is : ");
+        while(t!=NULL)
+        {
+            printf("%d\t",t->data);
+            t=t->next;
+        }
+    }
+}
+
+void bubblesort()
+{
+    
+}
+
+int main()
+{
+    int i,size;
+    printf("Enter the size of List :");
+    scanf("%d",&size);
+    
+    for(i=0;i<size;i++)
+    {
+        insert();
+    }
+
+    printf("The linked List is :");
+    display();
+    printf("\nArmstrong nodes form Linked list:");
+    armstrong();
+
+}
+
+
